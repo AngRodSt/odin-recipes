7c28f4b4bdafc09c26afbd5ab108404ec5eb313c
AngRod_Sth <sthefanyangeles00@gmail.com>
2023-11-26 14:57:36 -0400

    EjerciciosCss/

--- recipes/EjerciciosCss/Independiente/LandingPageV.1/Calculator/script.js
+++ recipes/EjerciciosCss/Independiente/LandingPageV.1/Calculator/script.js
@@ -6,6 +6,7 @@ const currentScreen = document.querySelector('#CurrentScreen')
 const equal = document.querySelector(".equal")
 const sign = document.querySelector(".sign")
 const porciento = document.querySelector(".porciento")
+const point = document.querySelector(".point")
 
 
 numbers.forEach((button)=>{
@@ -18,11 +19,23 @@ equal.addEventListener('click',()=>EqualFuncion())
 clear.addEventListener('click',()=>clearF())
 sign.addEventListener('click', ()=>signF())
 porciento.addEventListener('click',()=>porcientoF())
+point.addEventListener('click', ()=>punto())
 
-var FirstNumber= ''
-var SecondNumber = ''
+var FirstNumber= 0
+var SecondNumber = 0
 var OpResult = null
+var reset =false
 
+
+function resett(){
+    currentScreen.textContent=""
+   reset=false;
+}
+function punto (){
+    if(reset)return resett()
+    if (currentScreen.textContent.includes("."))return
+    currentScreen.textContent+="."
+}
 const porcientoF = ()=>{
     var a = Number(currentScreen.textContent)
     currentScreen.textContent = a / 100
@@ -42,24 +55,33 @@ const clearF= ()=>{
 }
 
 const EqualFuncion = ()=>{
+    if(OpResult==null){ return currentScreen.textContent=""}
+    if(lastScreen.textContent==""){return currentScreen.textContent=0}
     SecondNumber = currentScreen.textContent
-    currentScreen.textContent=FuncionOperacion(FirstNumber, OpResult,SecondNumber)
+    currentScreen.textContent= round(FuncionOperacion(FirstNumber, OpResult,SecondNumber))
     lastScreen.textContent = `${FirstNumber}${OpResult}${SecondNumber}`
+    OpResult=null;
+    reset=true;
+    
     
 }
 
 const ApperNumber = (number)=>{
-       if(currentScreen!=""){
-        currentScreen.textContent+=number
-       }
+    if(currentScreen.textContent==0 ||reset){resett()}
+        currentScreen.textContent+=number    
     }
+
 const ApperOperator = (operation)=>{
-        if (OpResult!==null){}
+        if (OpResult!=null)evaluate()
         FirstNumber = currentScreen.textContent
         OpResult = operation
-        lastScreen.textContent = `${FirstNumber}${OpResult}`
-        currentScreen.textContent="";     
-}
+        lastScreen.textContent = `${FirstNumber}${OpResult}`;
+        currentScreen.textContent='';
+       }
+
+
+
+const round = (number)=>{return Math.round(number*1000)/1000}
 
 
 const FuncionSuma = (a,b)=>a+b;
@@ -68,7 +90,7 @@ const FuncionMultip = (a,b)=>a*b;
 const FuncionDiv = (a,b)=>a/b;
 
 
-//REVISAR FUNCION
+
 const FuncionOperacion = (ap,operator,ap2)=>{
     var a = Number(ap)
     var b = Number(ap2)
@@ -85,12 +107,15 @@ const FuncionOperacion = (ap,operator,ap2)=>{
        return FuncionDiv(a,b)
     }
 
-    
-
 }
+ function evaluate (){
+    SecondNumber = currentScreen.textContent
+    currentScreen.textContent= round(FuncionOperacion(FirstNumber, OpResult,SecondNumber))
+    lastScreen.textContent = `${FirstNumber}${OpResult}${SecondNumber}`
+    OpResult=null
+ }
 
 
-//NUMBERS HAVE TO BE MAX 4 DECIMAL NUMBERS AFTER THE POINT
 //IMPUT ON THE KEYBORD
-//DOBLE CLICK ON OPERATOR 
+// POINT CLICK-- CAN'T BE MORE THEN ONE
 
